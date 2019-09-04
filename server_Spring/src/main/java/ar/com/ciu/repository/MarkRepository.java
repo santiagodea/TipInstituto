38d8c82137c7a7fc9485a20fe6ed1fc4350206a4
santiagodea <sntgdeandrea@gmail.com>
2019-09-04 19:06:07 -0300

    markrepository

--- server_Spring/src/main/java/ar/com/ciu/repository/MarkRepository.java
+++ server_Spring/src/main/java/ar/com/ciu/repository/MarkRepository.java
@@ -1,5 +1,10 @@
 package ar.com.ciu.repository;
 
-public interface MarkRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import ar.com.ciu.model.Mark;
+
+@Repository
+public interface MarkRepository extends CrudRepository<Mark, Long> {		// Le indico el tipo de dato y la primary key
 }
