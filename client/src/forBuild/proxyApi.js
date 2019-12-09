3e42cd609e4d201f7053a8ad716a1e391b4a01b9
Santiago De Andrea <sntgdeandrea@gmail.com>
2019-12-09 13:02:38 -0300

    mod DC

--- client/src/forBuild/proxyApi.js
+++ client/src/forBuild/proxyApi.js
@@ -1,1 +1,7 @@
-export default 'http://localhost:8080'
+module.exports = {
+  API_BASE_URL: process.env.API_BASE_URL || "http://localhost:8080/",
+  here: {
+    id: "Zo2UBTaa0AJls5BZPgKT",
+    code: "-uxzhXs_Dzn3CJl8aq7GUw"
+  }
+}
