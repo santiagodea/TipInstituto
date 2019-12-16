4926b933cd151d01c39f1e5e6cd7e3018912adfe
Santiago De Andrea <sntgdeandrea@gmail.com>
2019-12-16 17:57:30 -0300

    merge con branch arreglado en clases para mod docker

--- client/src/forBuild/proxyApi.js
+++ client/src/forBuild/proxyApi.js
@@ -1,5 +1,5 @@
 module.exports = {
-  API_BASE_URL: process.env.API_BASE_URL || "http://localhost:8080/",
+  API_BASE_URL: process.env.API_BASE_URL || window._env_.API_BASE_URL,
   here: {
     id: "Zo2UBTaa0AJls5BZPgKT",
     code: "-uxzhXs_Dzn3CJl8aq7GUw"
--- client/src/forBuild/proxyApiGenerator.js
+++ client/src/forBuild/proxyApiGenerator.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var process = require('process');
 
-fs.writeFile("./src/forBuild/proxyApi.js","export default '" + process.env.PROXY_API +"'", function(err) {
+fs.writeFile("./src/forBuild/proxyApi.js","export default '" + (process.env.PROXY_API || window._env_.PROXY_API) +"'", function(err) {
     if(err) {
         return console.log(err);
     } else {
