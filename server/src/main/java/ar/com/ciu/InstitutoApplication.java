d6ff81d38d904b52f21566429d1c4d01f4980c3f
Santiago De Andrea <sntgdeandrea@gmail.com>
2020-02-10 20:12:47 -0300

    ...

--- server/src/main/java/ar/com/ciu/InstitutoApplication.java
+++ server/src/main/java/ar/com/ciu/InstitutoApplication.java
@@ -2,9 +2,19 @@ package ar.com.ciu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class InstitutoApplication {
+	
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "ESTE ES EL SERVER DEL INSTITUTO!";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(InstitutoApplication.class, args);
